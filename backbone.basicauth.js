92c8a367bfdec53112a5fb8972a33bbd0c092737
Luis Abreu <lmjabreu@gmail.com>
2013-07-04 11:21:06 +0100

    Added AMD support, documentation, fetch basic auth credentials from URL

--- backbone.basicauth.js
+++ backbone.basicauth.js
@@ -1,48 +1,67 @@
 /*!
- * backbone.basicauth.js v0.2.0
- * Copyright 2013, Tom Spencer (@fiznool)
+ * backbone.basicauth.js v0.3.0
+ *
+ * Automatically parses the model/collection.url for http auth credentials
+ *
+ * Copyright 2013, Tom Spencer (@fiznool), Luis Abreu (@lmjabreu)
  * backbone.basicauth.js may be freely distributed under the MIT license.
  */
- ;(function(window) {
+;( function ( root, factory ) {
+  // AMD module if available
+  if ( typeof define === 'function' && define.amd ) {
+    // AMD
+    define( 'Backbone.basicAuth', [ 'Underscore', 'Backbone' ], function BackboneBasicAuthDefineCallback ( Underscore, Backbone ) {
+      factory( root, Underscore, Backbone );
+    } );
+  } else {
+    // Browser global scope
+    factory( root );
+  }
+}( this, function BackboneBasicAuthFactory ( window, Underscore, BBone ) {
 
-  // Local copy of global variables
-  var _ = window._;
-  var Backbone = window.Backbone;
+  var _ = Underscore || window._;
+  var Backbone = BBone || window.Backbone;
   var btoa = window.btoa;
 
-  var token = null;
-
-  var encode = function(username, password) {
+  /**
+   * Returns a base64 encoded "user:pass" string
+   * @param  {string} username The http auth username
+   * @param  {string} password The http auth password
+   * @return {string}          The base64 encoded credentials pair
+   */
+  var encode = function encodeToken ( username, password ) {
     // Use Base64 encoding to create the authentication details
     // If btoa is not available on your target browser there is a polyfill:
     // https://github.com/davidchambers/Base64.js
-    return btoa(username + ':' + password);
+    return btoa( [ username, password ].join( ':' ) );
   };
 
-  // Store a copy of the original Backbone.sync
-  var originalSync = Backbone.sync;
-
-  // Override Backbone.sync for all future requests.
-  // If a token is present, set the Basic Auth header
-  // before the sync is performed.
-  Backbone.sync = function(method, model, options) {
-    if (typeof token !== "undefined" && token !== null) {
+  /**
+   * Override Backbone.*.prototype.sync
+   *
+   * If a token is present, set the Basic Auth header before the sync is performed.
+   *
+   * @param  {string} method  Contains the request method. e.g.: GET, POST, etc
+   * @param  {object} context A Backbone model or collection
+   * @param  {object} options Options to be passed over to Backbone.sync and jQuery
+   * @return {object}         Reference to Backbone.sync for chaining
+   */
+  Backbone.Collection.prototype.sync = Backbone.Model.prototype.sync = function backboneSyncOverride ( method, context, options ) {
+    // Retrieve the auth credentials from the context url
+    var credentials = context.url.match( /\/\/(.*):(.*)@/ ),
+        token;
+    // Set the token if available
+    if ( credentials && credentials.length === 3 ) {
+      token = encode( credentials[ 1 ], credentials[ 2 ] );
+    }
+    // Add the token to the request headers if available
+    if ( typeof token !== 'undefined' && token !== null ) {
       options.headers = options.headers || {};
-      _.extend(options.headers, { 'Authorization': 'Basic ' + token });
+      _.extend( options.headers, { 'Authorization': 'Basic ' + token } );
     }
-    return originalSync.call(model, method, model, options);
+
+    // Perform the sync
+    return Backbone.sync.apply( this, arguments );
   };
 
-  Backbone.BasicAuth = {
-    // Setup Basic Authentication for all future requests
-    set: function(username, password) {
-      token = encode(username, password);
-    },
-
-    // Clear Basic Authentication for all future requests
-    clear: function() {
-      token = null;
-    }
-  };
-
-})(this);
+} ) );
